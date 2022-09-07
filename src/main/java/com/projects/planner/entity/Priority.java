de1f79d65341a257dee396afe69b848c524781e6
IlyaVostrukhin <i.s.vostrukhin@yandex.ru>
2022-09-07 19:51:15 +0700

    Added classes for work with Priority

--- src/main/java/com/projects/planner/entity/Priority.java
+++ src/main/java/com/projects/planner/entity/Priority.java
@@ -1,6 +1,5 @@
 package com.projects.planner.entity;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
